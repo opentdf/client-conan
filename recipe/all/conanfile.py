83d21f7813796ebe490ccf18c202d1c416141656
Pat Mancuso <46453397+patmantru@users.noreply.github.com>
2022-03-22 16:35:12 -0400

    [PLAT-1626] fix sample cpp error (#11)
    * Add options to allow using branch name as version and using virtru or opentdf repo
    
    * Fix 0.6.2 build
    
    * Tweak version minimums for virtru build
    
    * Remove pre-BSD-license versions

--- recipe/all/conanfile.py
+++ recipe/all/conanfile.py
@@ -11,11 +11,11 @@ class OpenTDFConan(ConanFile):
     homepage = "https://www.virtru.com"
     topics = ("opentdf", "opentdf-client", "tdf", "virtru")
     description = "openTDF core c++ client library for creating and accessing TDF protected data"
-    license = "MIT"
+    license = "BSD-3-Clause-Clear"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"build_python": [True, False], "fPIC": [True, False]}
-    default_options = {"build_python": False, "fPIC": True, "libzip:with_openssl": False, "libarchive:with_zlib":False}
+    options = {"build_python": [True, False], "fPIC": [True, False], "without_libiconv": [True, False], "without_zlib": [True, False], "branch_version": [True, False], "branch_repo": ["client-cpp", "tdf3-cpp"]}
+    default_options = {"build_python": False, "fPIC": True, "without_libiconv": False, "without_zlib": False, "branch_version": False, "branch_repo": "client-cpp"}
     exports_sources = ["CMakeLists.txt"]
 
     _cmake = None
@@ -30,15 +30,15 @@ class OpenTDFConan(ConanFile):
 
     @property
     def _minimum_cpp_standard(self):
-        return 14
+        return 17
 
     @property
     def _minimum_compilers_version(self):
         return {
-            "Visual Studio": "17",
-            "gcc": "9",
+            "Visual Studio": "15",
+            "gcc": "7.0.0",
             "clang": "12",
-            "apple-clang": "12.0.0",
+            "apple-clang": "12.0.0"
         }
 
     def validate(self):
@@ -46,30 +46,27 @@ class OpenTDFConan(ConanFile):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+            self.output.warn("{} recipe could not find compiler minimum version for {}.".format(
                 self.name, self.settings.compiler))
         else:
             if tools.Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
-                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+                raise ConanInvalidConfiguration("{} recipe needs minimum version {} but found tools.Version({}) = {}".format(
+                    self.name, min_version, self.settings.compiler.version, tools.Version(self.settings.compiler.version) ))
 
     def configure(self):
-        if str(self.settings.arch).startswith('arm'):
-            self.options["openssl"].no_asm = True
+        if self.options.without_zlib:
             self.options["libxml2"].zlib = False
-            self.options["libxml2"].lzma = False
-            self.options["libxml2"].icu = False
+        if self.options.without_libiconv:
+            self.options["boost"].without_locale = True
+            self.options["boost"].without_log = True
+            self.options["libarchive"].with_iconv = False
 
     def requirements(self):
         self.requires("openssl/1.1.1l@")
-        if str(self.settings.arch).startswith('arm'):
-            self.requires("boost/1.74.0@")
-        else:
-            self.requires("boost/1.76.0@")
-        self.requires("zlib/1.2.11@")
+        self.requires("boost/1.76.0@")
         self.requires("ms-gsl/2.1.0@")
         self.requires("libxml2/2.9.10@")
-        self.requires("libarchive/3.5.2@")
+        self.requires("libarchive/3.5.1@")
         self.requires("nlohmann_json/3.10.4@")
         self.requires("jwt-cpp/0.4.0@")
 
@@ -78,7 +75,14 @@ class OpenTDFConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        if self.options.branch_version:
+            self.output.warn("branch_repo = {}".format(self.options.branch_repo))
+            if self.options.branch_repo == "tdf3-cpp":
+                self.run("git clone git@github.com:virtru/tdf3-cpp.git --depth 1 --branch " + self.version + " " + self._source_subfolder)
+            else:
+                self.run("git clone git@github.com:opentdf/client-cpp.git --depth 1 --branch " + self.version + " " + self._source_subfolder)
+        else:
+            tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -104,5 +108,5 @@ class OpenTDFConan(ConanFile):
         self.cpp_info.components["libopentdf"].names["cmake_find_package"] = "opentdf-client"
         self.cpp_info.components["libopentdf"].names["cmake_find_package_multi"] = "opentdf-client"
         self.cpp_info.components["libopentdf"].names["pkg_config"] = "opentdf-client"
-        self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "zlib::zlib", "ms-gsl::ms-gsl", "libxml2::libxml2", "libarchive::libarchive", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json"]
+        self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "ms-gsl::ms-gsl", "libxml2::libxml2", "libarchive::libarchive", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json"]
 
